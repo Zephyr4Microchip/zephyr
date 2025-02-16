d882327b7ef4877dac8ffe63c28c5ddc8034810c
Mohamed Azhar <mohamed.azhar@microchip.com>
2025-02-17 00:29:51 +0530

    drivers: pinctrl: microchip: add pinctrl driver
    Updated with Doxygen style comments
    
    Signed-off-by: Mohamed Azhar <mohamed.azhar@microchip.com>

--- drivers/pinctrl/pinctrl_mchp_v1.c
+++ drivers/pinctrl/pinctrl_mchp_v1.c
@@ -1,12 +1,29 @@
 /*
- * Copyright (c) [2025] [Microchip Technology Inc.]
+ * Copyright (c) 2025 Microchip Technology Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file pinctrl_mchp_v1.c
+ * @brief Pin control driver for Microchip devices.
+ *
+ * This file provides the implementation of pin control functions
+ * for Microchip-based systems.
  */
 
 #include <zephyr/drivers/pinctrl.h>
 #include "pinctrl_mchp_v1.h"
 
+/**
+ * @brief Configure a specific pin based on the provided pin configuration.
+ *
+ * This helper function configures a pin by determining its port function and then
+ * calling the appropriate hardware abstraction layer (HAL) functions to set
+ * the pinmux and other pin configurations.
+ *
+ * @param pin The pin configuration to be applied. This is of type pinctrl_soc_pin_t.
+ */
 static void pinctrl_configure_pin(pinctrl_soc_pin_t pin)
 {
 	uint8_t port_func;
@@ -21,6 +38,19 @@ static void pinctrl_configure_pin(pinctrl_soc_pin_t pin)
 	hal_mchp_pinctrl_set_flags(&pin, mchp_port_addrs);
 }
 
+/**
+ * @brief Configure multiple pins.
+ *
+ * This function configures a set of pins based on the provided pin
+ * configuration array.
+ *
+ * @param pins Pointer to an array of pinctrl_soc_pin_t structures that
+ *             define the pin configurations.
+ * @param pin_cnt Number of pins to configure.
+ * @param reg Unused parameter.
+ *
+ * @return 0 on success.
+ */
 int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
 {
 	ARG_UNUSED(reg);
--- drivers/pinctrl/pinctrl_mchp_v1.h
+++ drivers/pinctrl/pinctrl_mchp_v1.h
@@ -1,24 +1,41 @@
 /*
- * Copyright (c) [2025] [Microchip Technology Inc.]
+ * Copyright (c) 2025 Microchip Technology Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/** @file
+/**
+ * @file pinctrl_mchp_v1.h
  * @brief Microchip MCU family I/O Pin Controller (PORT)
  */
 
 #ifndef ZEPHYR_DRIVERS_PINCTRL_PINCTRL_MCHP_V1_H_
 #define ZEPHYR_DRIVERS_PINCTRL_PINCTRL_MCHP_V1_H_
+
 #include <soc.h>
 
-/** Utility macro that expands to the PORT port address if it exists */
+/**
+ * @brief Utility macro that expands to the PORT address if it exists.
+ *
+ * This macro checks if a node label exists in the device tree and, if it does,
+ * it expands to the register address of that node label. If the node label does
+ * not exist, it expands to nothing.
+ *
+ * @param nodelabel The node label to check in the device tree.
+ */
 #define MCHP_PORT_ADDR_OR_NONE(nodelabel)                                                          \
-	IF_ENABLED(DT_NODE_EXISTS(DT_NODELABEL(nodelabel)),			\
-		   (DT_REG_ADDR(DT_NODELABEL(nodelabel)),))
+	IF_ENABLED(DT_NODE_EXISTS(DT_NODELABEL(nodelabel)),            \
+		(DT_REG_ADDR(DT_NODELABEL(nodelabel)),))
 
 #if defined(CONFIG_SOC_SERIES_MCHP_SAME54)
 
+/**
+ * @brief Array of port addresses for the MCHP SAME54 series.
+ *
+ * This array contains the register addresses of the ports (PORTA, PORTB, PORTC, and PORTD)
+ * for the MCHP SAME54 series microcontrollers. The addresses are obtained using the
+ * MCHP_PORT_ADDR_OR_NONE macro, which ensures that only existing ports are included.
+ */
 static const uint32_t mchp_port_addrs[] = {
 	MCHP_PORT_ADDR_OR_NONE(porta),
 	MCHP_PORT_ADDR_OR_NONE(portb),
