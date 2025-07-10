de7522323fac424b3144d0eb828603e3a982f2f3
Arunprasath P <arunprasath.p@microchip.com>
2025-07-10 19:11:36 +0530

    soc: microchip: add support for SAM D5x/E5x SoC series
    Adds initial SoC-level support for the Microchip SAM D5x
    and E5x series, including SoC definition files.
    
    Signed-off-by: Arunprasath P <arunprasath.p@microchip.com>

---
+++ soc/microchip/sam/sam_d5x_e5x/common/soc.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+
+void soc_reset_hook(void)
+{
+	/* Intentionally empty */
+}
---
+++ soc/microchip/sam/sam_d5x_e5x/same54/soc.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Microchip Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_MICROCHIP_SAM_D5X_E5X_SAME54_SOC_H_
+#define SOC_MICROCHIP_SAM_D5X_E5X_SAME54_SOC_H_
+
+#ifndef _ASMLANGUAGE
+
+#define DONT_USE_CMSIS_INIT
+
+#include <zephyr/types.h>
+
+#if defined(CONFIG_SOC_ATSAME54N19A)
+#include <same54n19a.h>
+#elif defined(CONFIG_SOC_ATSAME54N20A)
+#include <same54n20a.h>
+#elif defined(CONFIG_SOC_ATSAME54P19A)
+#include <same54p19a.h>
+#elif defined(CONFIG_SOC_ATSAME54P20A)
+#include <same54p20a.h>
+#else
+#error Library does not support the specified device.
+#endif
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* SOC_MICROCHIP_SAM_D5X_E5X_SAME54_SOC_H_ */
