e4df955fb5b2efee7df0eb276d76995b10b9e29d
Ella Chu <ella.chu@microchip.com>
2025-10-01 16:05:57 +0800

    board: microchip: add pic32wm_bz6204 curiosity board support
    Add initial support for the Microchip pic32wm_bz6204 curiosity board
    (pic32wm_bz6204_curiosity)
    
    Signed-off-by: Ella Chu <ella.chu@microchip.com>

---
+++ boards/microchip/pic32wm/pic32wm_bz6204_curiosity/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2025 Microchip Technology Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=pic32wm_bz6204" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
