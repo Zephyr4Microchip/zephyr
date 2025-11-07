0c2dd935844e5ebef125bb74b7652b5e58d3833c
Dinesh Arasu <dinesh.arasu@microchip.com>
2025-11-07 13:58:54 +0530

    boards: wbz450_curiosity: updated the board.cmake
    Commented the jlink runner
    
    Signed-off-by: Dinesh Arasu <dinesh.arasu@microchip.com>

--- boards/microchip/wbz/wbz450_curiosity/board.cmake
+++ boards/microchip/wbz/wbz450_curiosity/board.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2025 Microchip Technology Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=wbz450" "--speed=4000")
+#board_runner_args(jlink "--device=wbz450" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+#include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
